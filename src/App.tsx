e592ad5d424631586341e84596bb92ead5f5a4a6
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-04-05 18:33:38 -0400

    prod: add react-router and boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route, Navigate } from "react-router";
+
 const App: React.FC = (): React.ReactElement => {
-  return <div>Hello World!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={"/home"} />} />
+      <Route path="/home" element={<div>Hello World!</div>} />
+      <Route path="/*" element={<Navigate to={"/"} />} />
+    </Routes>
+  );
 };
 
 export default App;
