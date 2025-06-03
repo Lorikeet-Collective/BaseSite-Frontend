acb8d15849a8c8052dee844eed6c85d92b88a88a
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-02 20:16:35 -0400

    create: add basic footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,31 @@
 import styles from "./Footer.module.css";
 
 const Footer: React.FC = (): React.ReactElement => {
-  return <footer className={styles.footerContainer}></footer>;
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerTitleSection}>
+        <h2 className={styles.footerOrgName}>Lorikeet Collective, PLLC.</h2>
+      </div>
+      <div className={styles.footerSection}>
+        <h3 className={styles.footerTitle}>Location</h3>
+        <p>1 Liberty Square</p>
+        <p>Suite 301</p>
+        <p>New Britain, CT 06052</p>
+      </div>
+      <ul className={styles.footerSection}>
+        <h3 className={styles.footerTitle}>Links</h3>
+        <li className={styles.footerLink}>
+          <a href="null">Contact</a>
+        </li>
+        <li className={styles.footerLink}>
+          <a href="null">Help Squash Bugs</a>
+        </li>
+        <li className={styles.footerLink}>
+          <a href="null">Privacy Policies</a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
