d0e75c276203ad073638b05655d2285aa1bca200
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-05 16:30:37 -0400

    iter: continue work on footer design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,13 +4,13 @@ const Footer: React.FC = (): React.ReactElement => {
   return (
     <footer className={styles.footerContainer}>
       <h2 className={styles.footerOrgName}>Lorikeet Collective, PLLC.</h2>
-      <div className={styles.footerSection}>
+      <div className={styles.footerSectionAddress}>
         <h3 className={styles.footerTitle}>Location</h3>
         <p>1 Liberty Square</p>
         <p>Suite 301</p>
         <p>New Britain, CT 06052</p>
       </div>
-      <ul className={(styles.footerSection, styles.footerSectionLinks)}>
+      <ul className={styles.footerSectionLinks}>
         <h3 className={styles.footerTitle}>Links</h3>
         <li className={styles.footerLink}>
           <a href="null">Contact</a>
