fc297488172bae24523d8d3a34bdcbc3ee23886b
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-08 18:28:24 -0400

    change: update css in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,32 @@
+import { Link } from "react-router";
 import styles from "./Footer.module.css";
 
 const Footer: React.FC = (): React.ReactElement => {
   return (
     <footer className={styles.footerContainer}>
       <h2 className={styles.footerOrgName}>Lorikeet Collective, PLLC.</h2>
-      <div className={styles.footerSectionAddress}>
-        <h3 className={styles.footerTitle}>Location</h3>
+      <div className={`${styles.footerSection} ${styles.footerSectionAddress}`}>
+        <h3 className={styles.footerHeader}>Location</h3>
         <p>1 Liberty Square</p>
         <p>Suite 301</p>
         <p>New Britain, CT 06052</p>
       </div>
-      <ul className={styles.footerSectionLinks}>
-        <h3 className={styles.footerTitle}>Links</h3>
-        <li className={styles.footerLink}>
-          <a href="null">Contact</a>
+      <ul className={`${styles.footerSection} ${styles.footerSectionLinks}`}>
+        <h3 className={styles.footerHeader}>Links</h3>
+        <li className={styles.footerLinkContainer}>
+          <Link className={styles.footerLink} to={"null"}>
+            Contact
+          </Link>
         </li>
-        <li className={styles.footerLink}>
-          <a href="null">Help Squash Bugs</a>
+        <li className={styles.footerLinkContainer}>
+          <Link className={styles.footerLink} to={"null"}>
+            Help Squash Bugs
+          </Link>
         </li>
-        <li className={styles.footerLink}>
-          <a href="null">Privacy Policies</a>
+        <li className={styles.footerLinkContainer}>
+          <Link className={styles.footerLink} to={"null"}>
+            Privacy Policies
+          </Link>
         </li>
       </ul>
     </footer>
