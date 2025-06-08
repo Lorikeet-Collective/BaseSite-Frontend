ed0eeb82a1ab5cf0e6381bee4a032fc7a7d4a5db
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-08 18:41:22 -0400

    change: update header styles and layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,26 +2,27 @@ import { Link } from "react-router";
 import MenuModal from "../MenuModal/MenuModal";
 import styles from "./Header.module.css";
 
-const Header: React.FC = (): React.ReactElement => {
-  return (
-    <header className={styles.headerContainer}>
-      <div>
-        <h1 className={styles.headerName}>
-          <Link to="/home">Lorikeet Collective</Link>
-        </h1>
-        <img src="null" alt="logo" />
-      </div>
-      <button className={styles.bookBtn}>
-        <a
-          href="https://lorikeetcollective.sessionshealth.com/"
-          target="_blank"
-        >
-          BOOK NOW
-        </a>
-      </button>
-      <MenuModal />
-    </header>
-  );
-};
+const Header: React.FC = (): React.ReactElement => (
+  <header className={styles.headerContainer}>
+    <div className={styles.orgContainer}>
+      <h1 className={styles.orgName}>
+        <Link className={styles.orgLink} to="/home">
+          Lorikeet Collective
+        </Link>
+      </h1>
+      <img src="null" alt="logo" />
+    </div>
+    <button className={styles.bookBtn}>
+      <a
+        className={styles.bookBtnLink}
+        href="https://lorikeetcollective.sessionshealth.com/"
+        target="_blank"
+      >
+        BOOK NOW
+      </a>
+    </button>
+    <MenuModal />
+  </header>
+);
 
 export default Header;
