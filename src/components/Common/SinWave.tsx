bf294753b779faef21e3e18669ceca8fd9ce7fb8
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-12 18:16:10 -0400

    fix: adjust sinwave to correctly use props on load

--- src/components/Common/SinWave.tsx
+++ src/components/Common/SinWave.tsx
@@ -7,6 +7,20 @@ interface SinWaveProps {
   frequency?: number;
 }
 
+const generateSineWavePath = ({
+  width,
+  height,
+  amplitude,
+  frequency,
+}: SinWaveProps): string => {
+  let path: string = "";
+  for (let i = 0; i <= width!; i++) {
+    const y = height! / 2 + amplitude! * Math.sin(i * frequency!);
+    path += `${i},${y} `;
+  }
+  return `L${path}`;
+};
+
 const SineWave: React.FC<SinWaveProps> = ({
   width = window.innerWidth,
   height = 25,
@@ -20,35 +34,22 @@ const SineWave: React.FC<SinWaveProps> = ({
     frequency,
   });
 
-  // Development rerendering function
-  useEffect(
-    () => setSinData({ width, height, amplitude, frequency }),
-    [width, height, amplitude, frequency]
-  );
-
   useEffect(() => {
     const event: EventListener = (e: Event) => {
       const target = e.target as Window;
-      setSinData((prevData) => ({ ...prevData, width: target.innerWidth }));
+      setSinData((prevData) => ({
+        ...prevData,
+        width: target.innerWidth,
+      }));
     };
     addEventListener("resize", event);
     return () => removeEventListener("resize", event);
   }, []);
 
-  const generateSineWavePath = (): string => {
-    let path: string = "";
-    for (let i = 0; i <= sinData.width!; i++) {
-      const y =
-        sinData.height! / 2 +
-        sinData.amplitude! * Math.sin(i * sinData.frequency!);
-      path += `${i},${y} `;
-    }
-    return `L${path}`;
-  };
-
-  const pathData: string = `M0,${
-    sinData.height! / 2
-  } ${generateSineWavePath()}`;
+  useEffect(
+    () => setSinData({ width, height, frequency, amplitude }),
+    [width, height, frequency, amplitude]
+  );
 
   return (
     <svg
@@ -56,7 +57,11 @@ const SineWave: React.FC<SinWaveProps> = ({
       height={sinData.height}
       viewBox={`0 0 ${sinData.width} ${sinData.height}`}
     >
-      <path d={pathData} fill="none" stroke={`var(--tertiary-color)`} />
+      <path
+        d={`M0,${height / 2} ${generateSineWavePath(sinData)}`}
+        fill="none"
+        stroke={`var(--tertiary-color)`}
+      />
     </svg>
   );
 };
