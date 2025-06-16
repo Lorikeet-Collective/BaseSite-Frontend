27f09dbca1393eb560ee38793185944e03293ada
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-16 16:23:15 -0400

    fix: change animation to logic based

--- src/components/Common/Dropdown.tsx
+++ src/components/Common/Dropdown.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
-import { useState, useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import { upCarot, downCarot } from "../SVG/icons";
 import SVGIcon from "../SVG/SVGIcon";
 import styles from "./Dropdown.module.css";
@@ -14,18 +14,62 @@ const Dropdown: React.FC<DropdownProps> = ({
   body = [],
   children,
 }): React.ReactElement => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isRendered, setIsRendered] = useState(false);
   const dropdown = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [calcHeight, setCalcHeight] = useState<number>(0);
+  const [animationEffect] = useState<{
+    open: Animation | null;
+    close: Animation | null;
+  }>({ open: null, close: null });
+
+  useEffect(() => {
+    setCalcHeight(dropdown.current?.clientHeight as number);
+  }, []);
+
+  useEffect(() => {
+    if (!animationEffect.open && !animationEffect.close && calcHeight !== 0) {
+      const animationOpen = new KeyframeEffect(
+        dropdown.current,
+        [
+          { height: "0", opacity: "0" },
+          { height: `${calcHeight + 10}px`, opacity: "1" },
+        ],
+        {
+          duration: 500,
+          fill: "forwards",
+        }
+      );
+      const animationClose = new KeyframeEffect(
+        dropdown.current,
+        [
+          { height: `${calcHeight + 10}px`, opacity: "1" },
+          { height: "0", opacity: "0" },
+        ],
+        {
+          duration: 500,
+          fill: "forwards",
+        }
+      );
+      animationEffect.open = new Animation(animationOpen, document.timeline);
+      animationEffect.close = new Animation(animationClose, document.timeline);
+      dropdown.current!.style.height = "0px";
+    }
+  }, [calcHeight, animationEffect]);
+
+  useEffect(() => {
+    if (isOpen) animationEffect.open?.play();
+    else animationEffect.close?.play();
+    return () => {
+      animationEffect.open?.cancel();
+      animationEffect.close?.cancel();
+    };
+  }, [isOpen, animationEffect]);
 
   return (
     <div className={styles.dropdownContainer}>
       <div
         className={styles.titleContainer}
-        onClick={() => {
-          if (!isRendered) setIsRendered(true);
-          setIsOpen(!isOpen);
-        }}
+        onClick={() => setIsOpen((prev) => !prev)}
       >
         <h3 className={styles.title}>{title}</h3>
         <SVGIcon
@@ -35,27 +79,14 @@ const Dropdown: React.FC<DropdownProps> = ({
           color={"var(--tertiary-color)"}
         />
       </div>
-      {isRendered && (
-        <div
-          key={String(Math.random())}
-          ref={dropdown}
-          className={styles.bodyContainer}
-          style={{
-            animationDirection: isOpen ? "normal" : "reverse",
-          }}
-          onAnimationEnd={() => {
-            if (dropdown.current!.style.animationDirection === "reverse")
-              setIsOpen(false);
-          }}
-        >
-          {body.map((text, i) => (
-            <p className={styles.bodyText} key={i + Math.random()}>
-              {text}
-            </p>
-          ))}
-          {children}
-        </div>
-      )}
+      <div ref={dropdown} className={styles.bodyContainer}>
+        {body.map((text, i) => (
+          <p className={styles.bodyText} key={i + Math.random()}>
+            {text}
+          </p>
+        ))}
+        {children}
+      </div>
     </div>
   );
 };
