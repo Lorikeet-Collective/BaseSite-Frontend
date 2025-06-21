5418dc9f228d832919d1c62710306e79206d493d
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-21 16:15:47 -0400

    change: change to implicit returns where possible

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,19 @@ import Philosophy from "./views/Philosophy";
 import Insurance from "./views/Insurance";
 import Resources from "./views/Resources";
 
-const App: React.FC = (): React.ReactElement => {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to={"/home"} />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/philosophy" element={<Philosophy />} />
-        <Route path="/insurance" element={<Insurance />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/*" element={<Navigate to={"/"} />} />
-      </Routes>
-      <Footer />
-    </>
-  );
-};
+const App: React.FC = (): React.ReactElement => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Navigate to={"/home"} />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/philosophy" element={<Philosophy />} />
+      <Route path="/insurance" element={<Insurance />} />
+      <Route path="/resources" element={<Resources />} />
+      <Route path="/*" element={<Navigate to={"/"} />} />
+    </Routes>
+    <Footer />
+  </>
+);
 
 export default App;
--- src/views/Philosophy.tsx
+++ src/views/Philosophy.tsx
@@ -5,129 +5,126 @@ import Image from "../components/Common/Image";
 import Title from "../components/Common/Title";
 import Text from "../components/Common/Text";
 
-const Philosophy: React.FC = (): React.ReactElement => {
-  return (
-    <main>
-      <Title>Better therapy for clients and therapists</Title>
-      <BasicContainer>
-        <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1743928005549-U658USYAB90BEF90PHM8/unsplash-image-rIRM66unoW4.jpg?format=1500w" />
-        <Text>
-          Lorikeet Collective, PLLC. was founded on the principle that
-          therapeutic spaces should inclusive and affirming for all LGBTQAI2S+,
-          disabled, neurodiverse, BIPOC, and multiply marginalized folx— from
-          those who find themselves looking for a safe place to heal to those
-          called upon to heal others.
-        </Text>
-      </BasicContainer>
-      <SinWave />
-      <BasicContainer>
-        <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885154959-WRCI5BBUXBJ08HA8J0YO/unsplash-image-AqK0Sw2Gj-c.jpg?format=1500w" />
-        <Title subTitle>Beyond Talk</Title>
-        <Text>
-          Mental health care is more than having a professional to help guide
-          your journey. Truly equitable, accessible, and unifying therapy goes
-          beyond the one-hour a week commitment, which is why our therapists
-          take a community-first approach to treatment. Lorikeet Collective
-          therapists center holistic community care by pledging to engage in
-          political advocacy, facilitate mutual aid, and provide informed and
-          affirming continuing education.
-        </Text>
-      </BasicContainer>
-      <SinWave />
-      <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885072291-YHSA1IT3UCL0QH1HPXJW/unsplash-image-7XGtYefMXiQ.jpg?format=1500w" />
-      <Dropdown title="Which principles guide your therapeutic approach?">
-        <p>
-          Each clinician at Lorikeet Collective presents their own unique
-          background, identities, skills, and specialties. We are united by our
-          use of the healing justice framework, and common beliefs regarding
-          human rights that impact peoples’ quality of living on a constant
-          basis.
-        </p>
-        <ul>
-          <li>Black, brown, and indigenous lives matter.</li>
-          <li>There is no ethical consumption under capitalism.</li>
-          <li>Harm reduction saves lives.</li>
-          <li>
-            A person with a uterus has a right to choose an abortion and/or
-            permanent sterilization without limit or qualifying circumstances.
-          </li>
-          <li>Law enforcement does not belong in crisis care.</li>
-          <li>Believe in science.</li>
-          <li>
-            Conversion therapy (anti-LGBTQIA2S+ or applied behavior analysis/
-            ABA) is inherently traumatic and therefore, abuse.
-          </li>
-          <li>Decriminalize sex work.</li>
-          <li>Housing is a basic human right.</li>
-          <li>Nobody is illegal on stolen land.</li>
-          <li>Everyone is deserving and worthy of respect.</li>
-        </ul>
-      </Dropdown>
-      <Dropdown title={`What is "healing justice"?'`}>
-        <p>
-          As one of its creators, Cara Page defines healing justice as work
-          “that identifies how we can holistically respond to and intervene on
-          intergenerational trauma and violence.” In systemically oppressed
-          communities, healing justice is centered on drawing from historic
-          examples in which we have resisted the whims of oppressors to not only
-          survive, but thrive; this allows us to pay homage to the visionaries
-          and activists who led to where we are today and transform generational
-          trauma to facilitate collective healing as a community.
-        </p>
-      </Dropdown>
-      <Dropdown title="Which demographics do you server?">
-        <p>
-          As a collective comprised largely of neurodiverse, queer, and disabled
-          therapists, we utilize our unique intersection of lived experiences to
-          deliver affirming, informed evidence-based interventions to those
-          within our communities, across the lifespan.
-        </p>
-        <p>
-          While this is a community near and dear to our heart, the Lorikeet
-          Collective strives to promote inclusivity and accessibility. Our
-          services are available to individuals of all identities and
-          characteristics, and our clinicians pride themselves on their devotion
-          to improving competency and quality of care for communities
-          historically excluded from therapeutic spaces.
-        </p>
-      </Dropdown>
-      <Dropdown title="In which states are you licensed to provide care?">
-        <p>
-          A therapy collective is a non-hierarchical group of mental health
-          professionals. Therapy has the capability to heal and in a day and age
-          where therapists are expected to put others before themselves, it’s no
-          wonder burnout and turnover are not matters of “if,” but “when.”
-        </p>
-        <p>
-          Whereas traditional group practices function with a typical
-          supervisory hierarchy, here at Lorikeet Collective, our therapists are
-          all equal partners. This means more equitable wages, no long hours,
-          support in continued growth, and no productivity requirements.
-        </p>
-        <p>
-          Happy, empowered therapists provide more effective therapy that
-          produces a higher likelihood for positive client outcomes.
-        </p>
-      </Dropdown>
-      <Dropdown title="What's the meaning behind your name?">
-        <p>
-          The namesake of our collective, rainbow lorikeets, are a species of
-          parrot found in eastern Australia.
-        </p>
-        <p>
-          Aside from their beautifully vivid plumage which naturally flaunts
-          diversity and individuality, lorikeets are known for their tendency to
-          commune in flocks, displaying a strong sense of togetherness,
-          solidarity, and belonging. Lorikeets thrive in a variety of
-          environments, symbolizing adaptability and resilience.
-        </p>
-        <p>
-          Much like our community, lorikeets embody the message that love and
-          authenticity can flourish in the face of oppressive environments.{" "}
-        </p>
-      </Dropdown>
-    </main>
-  );
-};
+const Philosophy: React.FC = (): React.ReactElement => (
+  <main>
+    <Title>Better therapy for clients and therapists</Title>
+    <BasicContainer>
+      <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1743928005549-U658USYAB90BEF90PHM8/unsplash-image-rIRM66unoW4.jpg?format=1500w" />
+      <Text>
+        Lorikeet Collective, PLLC. was founded on the principle that therapeutic
+        spaces should inclusive and affirming for all LGBTQAI2S+, disabled,
+        neurodiverse, BIPOC, and multiply marginalized folx— from those who find
+        themselves looking for a safe place to heal to those called upon to heal
+        others.
+      </Text>
+    </BasicContainer>
+    <SinWave />
+    <BasicContainer>
+      <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885154959-WRCI5BBUXBJ08HA8J0YO/unsplash-image-AqK0Sw2Gj-c.jpg?format=1500w" />
+      <Title subTitle>Beyond Talk</Title>
+      <Text>
+        Mental health care is more than having a professional to help guide your
+        journey. Truly equitable, accessible, and unifying therapy goes beyond
+        the one-hour a week commitment, which is why our therapists take a
+        community-first approach to treatment. Lorikeet Collective therapists
+        center holistic community care by pledging to engage in political
+        advocacy, facilitate mutual aid, and provide informed and affirming
+        continuing education.
+      </Text>
+    </BasicContainer>
+    <SinWave />
+    <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885072291-YHSA1IT3UCL0QH1HPXJW/unsplash-image-7XGtYefMXiQ.jpg?format=1500w" />
+    <Dropdown title="Which principles guide your therapeutic approach?">
+      <p>
+        Each clinician at Lorikeet Collective presents their own unique
+        background, identities, skills, and specialties. We are united by our
+        use of the healing justice framework, and common beliefs regarding human
+        rights that impact peoples’ quality of living on a constant basis.
+      </p>
+      <ul>
+        <li>Black, brown, and indigenous lives matter.</li>
+        <li>There is no ethical consumption under capitalism.</li>
+        <li>Harm reduction saves lives.</li>
+        <li>
+          A person with a uterus has a right to choose an abortion and/or
+          permanent sterilization without limit or qualifying circumstances.
+        </li>
+        <li>Law enforcement does not belong in crisis care.</li>
+        <li>Believe in science.</li>
+        <li>
+          Conversion therapy (anti-LGBTQIA2S+ or applied behavior analysis/ ABA)
+          is inherently traumatic and therefore, abuse.
+        </li>
+        <li>Decriminalize sex work.</li>
+        <li>Housing is a basic human right.</li>
+        <li>Nobody is illegal on stolen land.</li>
+        <li>Everyone is deserving and worthy of respect.</li>
+      </ul>
+    </Dropdown>
+    <Dropdown title={`What is "healing justice"?'`}>
+      <p>
+        As one of its creators, Cara Page defines healing justice as work “that
+        identifies how we can holistically respond to and intervene on
+        intergenerational trauma and violence.” In systemically oppressed
+        communities, healing justice is centered on drawing from historic
+        examples in which we have resisted the whims of oppressors to not only
+        survive, but thrive; this allows us to pay homage to the visionaries and
+        activists who led to where we are today and transform generational
+        trauma to facilitate collective healing as a community.
+      </p>
+    </Dropdown>
+    <Dropdown title="Which demographics do you server?">
+      <p>
+        As a collective comprised largely of neurodiverse, queer, and disabled
+        therapists, we utilize our unique intersection of lived experiences to
+        deliver affirming, informed evidence-based interventions to those within
+        our communities, across the lifespan.
+      </p>
+      <p>
+        While this is a community near and dear to our heart, the Lorikeet
+        Collective strives to promote inclusivity and accessibility. Our
+        services are available to individuals of all identities and
+        characteristics, and our clinicians pride themselves on their devotion
+        to improving competency and quality of care for communities historically
+        excluded from therapeutic spaces.
+      </p>
+    </Dropdown>
+    <Dropdown title="In which states are you licensed to provide care?">
+      <p>
+        A therapy collective is a non-hierarchical group of mental health
+        professionals. Therapy has the capability to heal and in a day and age
+        where therapists are expected to put others before themselves, it’s no
+        wonder burnout and turnover are not matters of “if,” but “when.”
+      </p>
+      <p>
+        Whereas traditional group practices function with a typical supervisory
+        hierarchy, here at Lorikeet Collective, our therapists are all equal
+        partners. This means more equitable wages, no long hours, support in
+        continued growth, and no productivity requirements.
+      </p>
+      <p>
+        Happy, empowered therapists provide more effective therapy that produces
+        a higher likelihood for positive client outcomes.
+      </p>
+    </Dropdown>
+    <Dropdown title="What's the meaning behind your name?">
+      <p>
+        The namesake of our collective, rainbow lorikeets, are a species of
+        parrot found in eastern Australia.
+      </p>
+      <p>
+        Aside from their beautifully vivid plumage which naturally flaunts
+        diversity and individuality, lorikeets are known for their tendency to
+        commune in flocks, displaying a strong sense of togetherness,
+        solidarity, and belonging. Lorikeets thrive in a variety of
+        environments, symbolizing adaptability and resilience.
+      </p>
+      <p>
+        Much like our community, lorikeets embody the message that love and
+        authenticity can flourish in the face of oppressive environments.{" "}
+      </p>
+    </Dropdown>
+  </main>
+);
 
 export default Philosophy;
