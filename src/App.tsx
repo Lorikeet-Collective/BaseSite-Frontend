0578fea5268e989a5b83de6dba563455e678db73
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-21 19:03:07 -0400

    change: make pages scroll to top on location change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route, Navigate, useLocation } from "react-router";
+import { useEffect } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./views/Home";
@@ -6,19 +7,25 @@ import Philosophy from "./views/Philosophy";
 import Insurance from "./views/Insurance";
 import Resources from "./views/Resources";
 
-const App: React.FC = (): React.ReactElement => (
-  <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Navigate to={"/home"} />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/philosophy" element={<Philosophy />} />
-      <Route path="/insurance" element={<Insurance />} />
-      <Route path="/resources" element={<Resources />} />
-      <Route path="/*" element={<Navigate to={"/"} />} />
-    </Routes>
-    <Footer />
-  </>
-);
+const App: React.FC = (): React.ReactElement => {
+  const location = useLocation();
+
+  useEffect(() => window.scrollTo({ top: 0 }), [location.pathname]);
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to={"/home"} />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/philosophy" element={<Philosophy />} />
+        <Route path="/insurance" element={<Insurance />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/*" element={<Navigate to={"/"} />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
