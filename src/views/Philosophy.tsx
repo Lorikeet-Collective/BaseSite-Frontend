94049e4aa743c36dcd8362337804baa67738097d
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-21 16:12:05 -0400

    change: remove uneeded code

--- src/views/Philosophy.tsx
+++ src/views/Philosophy.tsx
@@ -8,12 +8,9 @@ import Text from "../components/Common/Text";
 const Philosophy: React.FC = (): React.ReactElement => {
   return (
     <main>
-      <Title title={"Better therapy for clients and therapists"} />
-      <BasicContainer
-        img={
-          "https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1743928005549-U658USYAB90BEF90PHM8/unsplash-image-rIRM66unoW4.jpg?format=1500w"
-        }
-      >
+      <Title>Better therapy for clients and therapists</Title>
+      <BasicContainer>
+        <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1743928005549-U658USYAB90BEF90PHM8/unsplash-image-rIRM66unoW4.jpg?format=1500w" />
         <Text>
           Lorikeet Collective, PLLC. was founded on the principle that
           therapeutic spaces should inclusive and affirming for all LGBTQAI2S+,
@@ -23,12 +20,9 @@ const Philosophy: React.FC = (): React.ReactElement => {
         </Text>
       </BasicContainer>
       <SinWave />
-      <BasicContainer
-        img={
-          "https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885154959-WRCI5BBUXBJ08HA8J0YO/unsplash-image-AqK0Sw2Gj-c.jpg?format=1500w"
-        }
-        header={"Beyond Talk"}
-      >
+      <BasicContainer>
+        <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885154959-WRCI5BBUXBJ08HA8J0YO/unsplash-image-AqK0Sw2Gj-c.jpg?format=1500w" />
+        <Title subTitle>Beyond Talk</Title>
         <Text>
           Mental health care is more than having a professional to help guide
           your journey. Truly equitable, accessible, and unifying therapy goes
@@ -40,12 +34,8 @@ const Philosophy: React.FC = (): React.ReactElement => {
         </Text>
       </BasicContainer>
       <SinWave />
-      <Image
-        img={
-          "https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885072291-YHSA1IT3UCL0QH1HPXJW/unsplash-image-7XGtYefMXiQ.jpg?format=1500w"
-        }
-      />
-      <Dropdown title={"Which principles guide your therapeutic approach?"}>
+      <Image img="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885072291-YHSA1IT3UCL0QH1HPXJW/unsplash-image-7XGtYefMXiQ.jpg?format=1500w" />
+      <Dropdown title="Which principles guide your therapeutic approach?">
         <p>
           Each clinician at Lorikeet Collective presents their own unique
           background, identities, skills, and specialties. We are united by our
@@ -73,7 +63,7 @@ const Philosophy: React.FC = (): React.ReactElement => {
           <li>Everyone is deserving and worthy of respect.</li>
         </ul>
       </Dropdown>
-      <Dropdown title={`What is "healing justice"?`}>
+      <Dropdown title={`What is "healing justice"?'`}>
         <p>
           As one of its creators, Cara Page defines healing justice as work
           “that identifies how we can holistically respond to and intervene on
@@ -85,7 +75,7 @@ const Philosophy: React.FC = (): React.ReactElement => {
           trauma to facilitate collective healing as a community.
         </p>
       </Dropdown>
-      <Dropdown title={"Which demographics do you server?"}>
+      <Dropdown title="Which demographics do you server?">
         <p>
           As a collective comprised largely of neurodiverse, queer, and disabled
           therapists, we utilize our unique intersection of lived experiences to
@@ -101,7 +91,7 @@ const Philosophy: React.FC = (): React.ReactElement => {
           historically excluded from therapeutic spaces.
         </p>
       </Dropdown>
-      <Dropdown title={"In which states are you licensed to provide care?"}>
+      <Dropdown title="In which states are you licensed to provide care?">
         <p>
           A therapy collective is a non-hierarchical group of mental health
           professionals. Therapy has the capability to heal and in a day and age
@@ -119,7 +109,7 @@ const Philosophy: React.FC = (): React.ReactElement => {
           produces a higher likelihood for positive client outcomes.
         </p>
       </Dropdown>
-      <Dropdown title={"What's the meaning behind your name?"}>
+      <Dropdown title="What's the meaning behind your name?">
         <p>
           The namesake of our collective, rainbow lorikeets, are a species of
           parrot found in eastern Australia.
