50181b24018c4af66b0c65e76edfc5cc1005a8d9
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-21 17:12:45 -0400

    iter: continue work on resources

--- src/views/Resources.tsx
+++ src/views/Resources.tsx
@@ -2,6 +2,7 @@ import Title from "../components/Common/Title";
 import Image from "../components/Common/Image";
 import Text from "../components/Common/Text";
 import Button from "../components/Common/Button";
+import SinWave from "../components/Common/SinWave";
 
 const Resources: React.FC = (): React.ReactElement => (
   <main>
@@ -40,12 +41,48 @@ const Resources: React.FC = (): React.ReactElement => (
         mandatory reporting, confidentiality, and security.
       </b>
     </Text>
+    <SinWave />
     <Title>Emergency Resources</Title>
     <Text>For immediate access to support when you need it. Age 18+.</Text>
     <Text>
       Looking for resources for under age 18? Scroll down to “For Kids & Teens.”
     </Text>
-    <Image fit src="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/8cccb2a2-c7fe-4ac1-ac44-2729fdd1bbbb/lgbt-branding_logo-horiz-large.png?format=1500w" />
+    <SinWave />
+    <Image
+      fit
+      src="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/8cccb2a2-c7fe-4ac1-ac44-2729fdd1bbbb/lgbt-branding_logo-horiz-large.png?format=1500w"
+    />
+    <Title subTitle>LGBT National Talkline and Coming Out Hotline:</Title>
+    <Text>
+      Call <a href="tel:8888434564">888-843-4564</a> for LGBT National Hotline
+      to access crisis support for ages 18+,{" "}
+      <a href="tel:8886885428">888-688-5428</a> for LGBT National Coming Out
+      Support Line for coming out support, or{" "}
+      <a href="tel:8882347243">888-234-7243</a> for LGBT National Senior Hotline
+      to access crisis support for ages 50+{" "}
+      <b>Mondays through Fridays 2pm-11pm EST and Saturdays 12pm-5pm EST</b>, or{" "}
+      <a href="https://volunteerlogin.org/chat/chat.php?groupChatTransferFlag=0&groupChatCallerId=H5aMQje1elT9u6eaL0YI">
+        chat one-on-one
+      </a>{" "}
+      with peers online.
+      <ul>
+        <li>
+          Live crisis, coming out, and{" "}
+          <a href="https://lgbthotline.org/self-harm-prevention/">
+            self-harm prevention/ safety planning
+          </a>{" "}
+          support for LGBTQIA2S+ of all ages across the USA
+        </li>
+        <li>
+          Offers information and definitions for folx looking to learn more
+          about sexuality and gender, as well as directories of books and film
+          for uplifting queer identities
+        </li>
+        <li>Confidential and anonymous support</li>
+      </ul>
+    </Text>
+
+    <SinWave />
   </main>
 );
 
