52726b4070f905d7798ba6f0de05dccd2cd25d17
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-22 16:41:28 -0400

    change: change images to local

--- src/views/Philosophy.tsx
+++ src/views/Philosophy.tsx
@@ -3,11 +3,15 @@ import Text from "../components/Common/Text";
 import Image from "../components/Common/Image";
 import SinWave from "../components/Common/SinWave";
 import Dropdown from "../components/Common/Dropdown";
+// Image imports
+import lorikeets from "../assets/lorikeets.jpg";
+import prideWalk from "../assets/pride-walk.jpg";
+import welcomeSign from "../assets/welcome-sign.jpg";
 
 const Philosophy: React.FC = (): React.ReactElement => (
   <main>
     <Title>Better therapy for clients and therapists</Title>
-    <Image src="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1743928005549-U658USYAB90BEF90PHM8/unsplash-image-rIRM66unoW4.jpg?format=1500w" />
+    <Image src={lorikeets} />
     <Text>
       Lorikeet Collective, PLLC. was founded on the principle that therapeutic
       spaces should inclusive and affirming for all LGBTQAI2S+, disabled,
@@ -16,7 +20,7 @@ const Philosophy: React.FC = (): React.ReactElement => (
       others.
     </Text>
     <SinWave />
-    <Image src="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885154959-WRCI5BBUXBJ08HA8J0YO/unsplash-image-AqK0Sw2Gj-c.jpg?format=1500w" />
+    <Image src={prideWalk} />
     <Title subTitle>Beyond Talk</Title>
     <Text>
       Mental health care is more than having a professional to help guide your
@@ -28,7 +32,7 @@ const Philosophy: React.FC = (): React.ReactElement => (
       continuing education.
     </Text>
     <SinWave />
-    <Image src="https://images.squarespace-cdn.com/content/v1/67d14d44c6a49e3d4d030b00/1745885072291-YHSA1IT3UCL0QH1HPXJW/unsplash-image-7XGtYefMXiQ.jpg?format=1500w" />
+    <Image src={welcomeSign} />
     <Dropdown title="Which principles guide your therapeutic approach?">
       <p>
         Each clinician at Lorikeet Collective presents their own unique
