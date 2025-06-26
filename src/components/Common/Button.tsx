27dc9e46abf6e4ccce732a50739c89d7167da32b
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-26 16:24:14 -0400

    change: modify button to be more reactive

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -3,26 +3,41 @@ import { Link } from "react-router";
 import styles from "./Button.module.css";
 
 interface ButtonProps extends PropsWithChildren {
-  link: string;
+  link?: string;
   external?: boolean;
+  isSmall?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
   link,
   external = true,
+  isSmall = false,
   children,
-}): React.ReactElement => (
-  <button className={styles.button}>
-    {external ? (
-      <a className={styles.buttonLink} href={link} target={"_blank"}>
+}): React.ReactElement => {
+  if (link) {
+    return (
+      <button className={styles.button}>
+        {external ? (
+          <a className={styles.buttonLink} href={link} target={"_blank"}>
+            {children}
+          </a>
+        ) : (
+          <Link className={styles.buttonLink} to={link}>
+            {children}
+          </Link>
+        )}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className={styles.button}
+        style={isSmall ? { width: "auto" } : {}}
+      >
         {children}
-      </a>
-    ) : (
-      <Link className={styles.buttonLink} to={link}>
-        {children}
-      </Link>
-    )}
-  </button>
-);
+      </button>
+    );
+  }
+};
 
 export default Button;
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,7 +1,44 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Button from "../Common/Button";
 // import styles from "./ContactForm.module.css";
 
 const ContactForm: React.FC = (): React.ReactElement => {
-  return <form></form>;
+  const form = useRef(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (form.current) {
+      emailjs
+        .sendForm("it_email", "contact_us", form.current, {
+          publicKey: "kvdRblQzkkoCjCkgd",
+        })
+        .then(() => console.log("SUCCESS!"))
+        .catch((error) => console.log("FAILED...", error.text));
+    }
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div>
+        <label>Subject</label>
+        <input type="text" name="user_title" />
+      </div>
+      <div>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="user_email" />
+      </div>
+      <div>
+        <label>Message</label>
+        <textarea name="user_message" />
+      </div>
+      <Button isSmall>SEND</Button>
+    </form>
+  );
 };
 
 export default ContactForm;
