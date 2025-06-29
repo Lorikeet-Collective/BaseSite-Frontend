2a73f5452cca0bb62808cc7fe391e08e61fe1f8a
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-29 15:13:58 -0400

    change: cleanup css in index

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,6 +1,5 @@
 import type { PropsWithChildren } from "react";
 import { Link } from "react-router";
-import styles from "./Button.module.css";
 
 interface ButtonProps extends PropsWithChildren {
   link?: string;
@@ -14,30 +13,19 @@ const Button: React.FC<ButtonProps> = ({
   isSmall = false,
   children,
 }): React.ReactElement => {
-  if (link) {
-    return (
-      <button className={styles.button}>
-        {external ? (
-          <a className={styles.buttonLink} href={link} target={"_blank"}>
-            {children}
-          </a>
-        ) : (
-          <Link className={styles.buttonLink} to={link}>
-            {children}
-          </Link>
-        )}
-      </button>
-    );
-  } else {
-    return (
-      <button
-        className={styles.button}
-        style={isSmall ? { width: "auto" } : {}}
-      >
-        {children}
-      </button>
-    );
-  }
+  return link ? (
+    <button>
+      {external ? (
+        <a href={link} target={"_blank"}>
+          {children}
+        </a>
+      ) : (
+        <Link to={link}>{children}</Link>
+      )}
+    </button>
+  ) : (
+    <button style={isSmall ? { width: "auto" } : {}}>{children}</button>
+  );
 };
 
 export default Button;
