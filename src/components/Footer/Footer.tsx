c3dc24a3fdd8ad8c062e12c8a8fba687ed673615
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-06-29 17:02:36 -0400

    change: simplify footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,29 +3,21 @@ import styles from "./Footer.module.css";
 
 const Footer: React.FC = (): React.ReactElement => (
   <footer className={styles.footerContainer}>
-    <h2 className={styles.footerOrgName}>Lorikeet Collective, PLLC.</h2>
-    <div className={`${styles.footerSection} ${styles.footerSectionAddress}`}>
-      <h3 className={styles.footerHeader}>Location</h3>
-      <p>1 Liberty Square</p>
-      <p>Suite 301</p>
-      <p>New Britain, CT 06052</p>
-    </div>
-    <ul className={`${styles.footerSection} ${styles.footerSectionLinks}`}>
-      <h3 className={styles.footerHeader}>Links</h3>
-      <li className={styles.footerLinkContainer}>
-        <Link className={styles.footerLink} to={"null"}>
-          Contact
-        </Link>
+    <h2>Lorikeet Collective, PLLC.</h2>
+    <h3>Location</h3>
+    <p>1 Liberty Square</p>
+    <p>Suite 301</p>
+    <p>New Britain, CT 06052</p>
+    <h3>Links</h3>
+    <ul>
+      <li>
+        <Link to={"null"}>Contact</Link>
       </li>
-      <li className={styles.footerLinkContainer}>
-        <Link className={styles.footerLink} to={"null"}>
-          Help Squash Bugs
-        </Link>
+      <li>
+        <Link to={"null"}>Help Squash Bugs</Link>
       </li>
-      <li className={styles.footerLinkContainer}>
-        <Link className={styles.footerLink} to={"null"}>
-          Privacy Policies
-        </Link>
+      <li>
+        <Link to={"null"}>Privacy Policies</Link>
       </li>
     </ul>
   </footer>
