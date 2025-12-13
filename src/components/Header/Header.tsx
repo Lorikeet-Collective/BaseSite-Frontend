f17517172e538531620ec8d804a5f9ebe0bb24fa
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-12-13 14:19:27 -0500

    change: diverify color range and add logo and favicon (temp)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,15 @@ import { Link } from "react-router";
 import MenuModal from "../MenuModal/MenuModal";
 import Button from "../Common/Button";
 import styles from "./Header.module.css";
+import lorikeetLogo from "../../assets/logos/lorikeet-logo.png";
 
 const Header: React.FC = (): React.ReactElement => (
   <header className={styles.headerContainer}>
     <div>
+      <img src={lorikeetLogo} alt="Lorikeet Collective logo" />
       <h1>
         <Link to="/home">Lorikeet Collective</Link>
       </h1>
-      {/* <img src="null" alt="logo" /> */}
     </div>
     <div>
       <Button isExternal link="https://lorikeetcollective.sessionshealth.com/">
