da0c3bbdc07efcd4a9d6d27b8818645fb0bdeeb7
kylerm <kylemorey1994@gmail.com>
2026-02-26 15:54:03 -0500

    change: migrate to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import "./desktop-index.css";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>
+    </BrowserRouter>,
 );
